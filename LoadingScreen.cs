43623b2057b30b96001b618376611e2b7b988355
Christian Brückl <christian.bru@lab132.com>
2024-04-03 16:17:09 +0200

    added resources and created loading screen

---
+++ LoadingScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BiomarktGmbH
+{
+    public partial class LoadingScreen : Form
+    {
+        public LoadingScreen()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
